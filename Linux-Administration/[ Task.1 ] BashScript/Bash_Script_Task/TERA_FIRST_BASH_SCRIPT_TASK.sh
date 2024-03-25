87bf1ac8771272cba14ecf718be79d278eda402d
TERA <127119775+mgtera200@users.noreply.github.com>
2024-03-25 02:39:42 +0200

    Delete Linux-Administration directory

--- Linux-Administration/[ Task.1 ] BashScript/Bash_Script_Task/TERA_FIRST_BASH_SCRIPT_TASK.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir ~/myDirectory
-mkdir ~/myDirectory/secondDirectory
-touch ~/myDirectory/secondDirectory/myNotePaper
-cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory/myOldNotePaper
